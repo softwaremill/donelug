a8462901e0466930dc33200b56f1bd98cca215db
lod <support@micwi.pl>
2014-11-28 16:51:22 +0100

    added rudimentary list processing

--- src/com/softwaremill/stash/donelug/DoneLugToolWindowFactory.java
+++ src/com/softwaremill/stash/donelug/DoneLugToolWindowFactory.java
@@ -3,8 +3,9 @@ package com.softwaremill.stash.donelug;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.components.JBList;
 
-import javax.swing.*;
+import java.util.Arrays;
 import java.util.Collection;
 
 /**
@@ -14,15 +15,13 @@ public class DoneLugToolWindowFactory implements ToolWindowFactory {
 
     @Override
     public void createToolWindowContent(Project project, ToolWindow toolWindow) {
-
-
-        toolWindow.getComponent().add(new JLabel("test"));
+        JBList list = new JBList(generateItems("","","",""));
+        toolWindow.getComponent().add(list);
     }
 
 
     private Collection<String> generateItems(String stashUrl, String repoName, String projectName, String pullRequestId) {
-        //TODO
-        return null;
+        return Arrays.asList("a","b","c","d");
     }
 
 
